21a5f77f95209c5fc41bdeeb437821195a324925
ManiLucas <57599828+ManiLucas@users.noreply.github.com>
2022-11-25 01:43:09 -0600

    Create update.php

---
+++ update.php
@@ -0,0 +1,30 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Product */
+
+$this->title = Yii::t('app', 'Update Product: {name}', [
+    'name' => $model->code,
+]);
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Products'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->code, 'url' => ['view', 'code' => $model->code]];
+$this->params['breadcrumbs'][] = Yii::t('app', 'Update');
+?>
+
+<div class="container-fluid">
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-12">
+                    <?=$this->render('_form', [
+                        'model' => $model
+                    ]) ?>
+                </div>
+            </div>
+        </div>
+        <!--.card-body-->
+    </div>
+    <!--.card-->
+</div>
+
+<!--.Github /var/www/html/classic/modules/admin/views/product/update.php-->
